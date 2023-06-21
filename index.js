7390fe75c0aac044a675fa6feaeb5560541c8809
christian7974 <ctropeano@uri.edu>
2023-06-21 18:19:06 -0400

    Selected Correct Divs in Skills
    Solved issue where I could not select the divs in Skills; can get them to appear/disappear when clicking button.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const langauges_skills_btn = document.getElementById("languages-btn");
-
 const frameworks_skills_btn = document.getElementById("frameworks-btn");
+const languages_div = document.getElementById("skills-in-languages");
+const frameworks_div = document.getElementById("skills-in-frameworks");
 
+console.log(frameworks_div);
 var on_languages = true;
 function showLanguagesDiv() {
-    console.log("showLanguagesDiv");
-    
+    frameworks_div.style.display = 'none';
+    languages_div.style.display = 'block';
 }
 
 function showFrameworksDiv() {
-    console.log("showFrameworksdiv");
+    languages_div.style.display = 'none';
+    frameworks_div.style.display = 'block';
 }
 langauges_skills_btn.addEventListener('click', showLanguagesDiv);
 frameworks_skills_btn.addEventListener('click', showFrameworksDiv);
