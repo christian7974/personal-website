40171de5baab18ee92311cd81e4e22efbd048a14
christian7974 <ctropeano@uri.edu>
2023-07-01 21:13:09 -0400

    Added Animation to About Me Div; Updated README
    Added fade-in animation to About Me section; Updated README to reflect adding animations

--- README.md
+++ README.md
@@ -10,11 +10,12 @@ I wanted to make this website after taking a Udemy Course about web development.
 
 ## Currently Working On
 
-One thing that I want to work on is to make my website responsive (look good on every device). I have been trying to make every section responsive on every device, however that has been very time consuming. I want to keep improving on that.
+One thing that I want to work on is to make my website responsive (look good on every device). I have been trying to make every section responsive on every device, however that has been very time consuming. I want to keep improving on that.  
+I also want to continue to make my website more than a static website by adding animations to keep it fresh.
 
 ## Things I Would Do Differently
 
-One thing that I would do differently would be to plan out what I would do in what order. I had the vision for my website (as seen in the prototypes folder), however I did not have the process planned out how I liked (I should have decided what sections I wanted to do first, what part of my website took priority, etc). While I am proud with how the website came out, planning out the steps would have allowed me to work on one section at a time rather than jump around.
+One thing that I would do differently would be to plan out what I would do in what order. I had the vision for my website (as seen in the prototypes folder), however I did not have the process planned out how I liked (I should have decided what sections I wanted to do first, what part of my website took priority, etc). While I am proud with how the website came out, planning out the steps would have allowed me to work on one section at a time rather than jump around.  
 I also want to come up with a way where when someone would enter their email and comment under the Contact Me section and I would receive an email that had their email as well as what they had to say. I had to settle with using a database to do this, however this seems like an overkill. It also means that I have to check periodically to make sure I did not miss someone saying anything.
 
 
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+var buttonPressed = false;
 
-
-const langauges_skills_btn = document.getElementById("languages-btn");
-const frameworks_skills_btn = document.getElementById("frameworks-btn");
-const languages_div = document.getElementById("skills-in-languages");
-const frameworks_div = document.getElementById("skills-in-frameworks");
+const languagesSkillsBtn = document.getElementById("languages-btn");
+const frameworksSkillsBtn = document.getElementById("frameworks-btn");
+const languagesDiv = document.getElementById("skills-in-languages");
+const frameworksDiv = document.getElementById("skills-in-frameworks");
 
 const languages_observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -12,18 +12,14 @@ const languages_observer = new IntersectionObserver(entries => {
         const rust_skill_bar = entry.target.querySelector("#rust-skill-bar");
         const html_skill_bar = entry.target.querySelector("#html-skill-bar");
         const js_skill_bar = entry.target.querySelector("#js-skill-bar");
-        const github_skill_bar = entry.target.querySelector("#github-skill-bar");
         if (entry.isIntersecting) {
             python_skill_bar.classList.add("python-animation");
             cpp_skill_bar.classList.add("cpp-animation");
             rust_skill_bar.classList.add("rust-animation");
             html_skill_bar.classList.add("html-animation");
             js_skill_bar.classList.add("js-animation");
-            github_skill_bar.classList.add("github-animation");
             return;
         }
-        // python_skill_bar.style.width = "70%";
-        // cpp_skill_bar.classList.remove("cpp-animation");
     })
 });
 
@@ -47,17 +43,29 @@ const frameworks_observer = new IntersectionObserver(entries => {
     })
 });
 
+const about_me_observer = new IntersectionObserver(entries => {
+    const about_me_content = document.querySelector("#about-me-content");
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            about_me_content.classList.add("fade-in");
+            return;
+        }
+
+    })
+});
+about_me_observer.observe(document.querySelector("#about-me"));
 function showLanguagesDiv() {
-    frameworks_div.style.display = 'none';
-    languages_div.style.display = 'block';
+    frameworksDiv.style.display = 'none';
+    languagesDiv.style.display = 'block';
 }
 
 function showFrameworksDiv() {
-    languages_div.style.display = 'none';
-    frameworks_div.style.display = 'block';
+    languagesDiv.style.display = 'none';
+    frameworksDiv.style.display = 'block';
 }
-langauges_skills_btn.addEventListener('click', showLanguagesDiv);
-frameworks_skills_btn.addEventListener('click', showFrameworksDiv);
+languagesSkillsBtn.addEventListener('click', showLanguagesDiv);
+frameworksSkillsBtn.addEventListener('click', showFrameworksDiv);
+
 
 languages_observer.observe(document.querySelector(".x-axis-languages"));
 frameworks_observer.observe(document.querySelector(".x-axis-frameworks"));
