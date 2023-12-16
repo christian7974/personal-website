8e2076d05e78f5e6d22327bfdd1350230992fd2f
christian7974 <ctropeano@uri.edu>
2023-12-16 17:05:59 -0500

    Began About Me Div
    Added content, made it responsive

---
+++ index.js
@@ -0,0 +1,4 @@
+// Function to alert the user that Arith is still being handed out (Github Repo is private)
+function arithLinkPressed() {
+    alert("This assignment is still being assigned - please email for more information.");
+}
